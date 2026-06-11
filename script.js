67e48ad685f205b9fa00e0116c949fabef8ae3cc
Maximo <maximoate2006@gmail.com>
2026-06-11 01:52:49 -0300

    act 5

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const reveals = document.querySelectorAll(".reveal");
 const gallertpreview = document.querySelectorAll(".gallery-preview");
 const imaginmput = document.querySelector(".imagin-input");
+const proyectsBtn = document.querySelector(".proyects-btn");
+const projectsNoreboock = document.querySelector(".projects-norebook");
+
+proyectsBtn.addEventListener("click", () => {
+    projectsNoreboock.classList.toggle("active");
+});
 
 window.addEventListener("scroll", () => {
 
@@ -16,22 +22,26 @@ window.addEventListener("scroll", () => {
 
 });
 
-imageInput.addEventListener('change', (event) => {
-    const files = event.target.files;
-    for (let i = 0; i < files.length; i++) {
-        const file = files[i];
-        
-        if (file.type.startsWith('image/')) {
-            const reader = new FileReader();
-            
-            reader.onload = function(e) {
-                const img = document.createElement('img');
-                img.src = e.target.result;
-                img.alt = "Evidencia de ATOM GYM";
-                galleryPreview.appendChild(img); // añadir imagen
-            };
-            
-            reader.readAsDataURL(file);
-        }
+function setupGallery(inputId, previewId) {
+    const input = document.getElementById(inputId);
+    const preview = document.getElementById(previewId);
+
+    if (input && preview) {
+        input.addEventListener('change', (event) => {
+            const files = event.target.files;
+            for (let i = 0; i < files.length; i++) {
+                const file = files[i];
+                if (file.type.startsWith('image/')) {
+                    const reader = new FileReader();
+                    reader.onload = function(e) {
+                        const img = document.createElement('img');
+                        img.src = e.target.result;
+                        img.alt = "Captura de evidencia del proyecto";
+                        preview.appendChild(img);
+                    };
+                    reader.readAsDataURL(file);
+                }
+            }
+        });
     }
-});
+}
