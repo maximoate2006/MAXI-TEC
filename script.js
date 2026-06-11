c255ab1176949c3333de7ac547d13a72e23d0b90
Maximo <maximoate2006@gmail.com>
2026-06-11 01:49:10 -0300

    act 3

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const reveals = document.querySelectorAll(".reveal");
+const gallertpreview = document.querySelectorAll(".gallery-preview");
+const imaginmput = document.querySelector(".imagin-input");
 
 window.addEventListener("scroll", () => {
 
@@ -12,4 +14,24 @@ window.addEventListener("scroll", () => {
 
     });
 
+});
+
+imageInput.addEventListener('change', (event) => {
+    const files = event.target.files;
+    for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        
+        if (file.type.startsWith('image/')) {
+            const reader = new FileReader();
+            
+            reader.onload = function(e) {
+                const img = document.createElement('img');
+                img.src = e.target.result;
+                img.alt = "Evidencia de ATOM GYM";
+                galleryPreview.appendChild(img); // añadir imagen
+            };
+            
+            reader.readAsDataURL(file);
+        }
+    }
 });
